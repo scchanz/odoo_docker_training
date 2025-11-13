202408788bb56a47f73ffd379e7908ebedb91844
scchanz <ohyunnss@gmail.com>
2025-11-13 15:46:44 +0700

    + publication date & pages

--- addons/library_management/models/library_book.py
+++ addons/library_management/models/library_book.py
@@ -8,6 +8,6 @@ class LibraryBook(models.Model):
     author = fields.Char(string="Author")
     isbn = fields.Char(string="ISBN")
     publication_date = fields.Date(string="Publication Date")
-    pages = fields.Integer(string="Number of Pages")
+    pages = fields.Integer(string="Pages")
     available = fields.Boolean(string="Available", default=True)
     description = fields.Text(string="Description")  # <-- Tambahkan ini
